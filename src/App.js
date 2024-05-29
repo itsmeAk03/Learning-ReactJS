621c5356c480039a07343153d5f025da903d0010
Anupam <anupambhadra9@gmail.com>
2024-05-29 20:31:05 +0530

    xyz

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import React,{ useState }  from 'react'; 
-// import './App.css';
+import './App.css';
 // import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
@@ -11,6 +11,8 @@ import Alert from './components/Alert';
 function App() {
   const[mode,setMode]=useState('light');
   const[alert, setAlert] = useState('alertmsg');
+  // const[col,myColor]=useState('blue');
+
 
   const showAlert=(message ,type)=>{
     setAlert({
@@ -18,16 +20,24 @@ function App() {
       type: type
     })
   }
+
+  // const btnColor=(rang)=>{
+  //   myColor({
+      
+  //   })
+  // }
   const toggleMode=()=>{
     if(mode==='light'){
     setMode('dark');
     document.body.style.backgroundColor='#042743';
     showAlert("Dark Mode has been enabled","success");
+   
     }
     else{
       setMode('light');
       document.body.style.backgroundColor= 'white';
       showAlert("Light Mode has been enabled","success");
+     
     }
   }
   return (
