5c142545331147bb6ef12f758a02abda11430dcb
Anupam <anupambhadra9@gmail.com>
2024-06-01 18:12:16 +0530

    textutils

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 
 import React,{ useState }  from 'react'; 
 import './App.css';
-// import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Routes
+  
+} from "react-router-dom";
+// import { Routes, Route , BrowserRouter as Router, Link} from 'react-router-dom';
 
 
 
@@ -61,12 +69,21 @@ function App() {
   }
   return (
       <>
+        <Router>
         <Navbar title="Textutils" aboutText="About" greenMode={greenMode} mode={mode} toggleMode={toggleMode} GreenMode={GreenMode} />
         <Alert alert={alert} />
         <div className="container my-3" >
-        <TextForm  showAlert={showAlert} heading="Enter the text " greenMode={greenMode} mode={mode} />
+        <Routes>
+            <Route path="/about" element={<About />}>
+            </Route>
+            <Route path="/" element={<TextForm heading="Enter text to analyze"  mode={mode} showAlert={showAlert} />}>
+            </Route>
+          </Routes>
+
+        
         </div>
-        {/* <About/> */}
+         {/*<About/>*/} 
+         </Router>
        
       </>
 
